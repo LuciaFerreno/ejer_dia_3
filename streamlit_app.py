01a7c46a1769e0654e0987761d12805347bd9635
Lucia Ferreño <lucia.ferreno@altia.es>
2026-04-06 16:35:17 +0200

    cambios para tabala editable

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,4 @@ archivo = st.file_uploader("Sube un CSV", type=["csv"])
 
 if archivo:
     df = pd.read_csv(archivo)
-    st.dataframe(df, use_container_width=True)
-    
+    st.experimental_data_editor(df, num_rows="dynamic")
