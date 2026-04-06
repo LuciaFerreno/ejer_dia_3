5b51e9414ff8debcd89ea40c7ccd2cfe6ff43417
Lucia Ferreño <lucia.ferreno@altia.es>
2026-04-06 16:58:32 +0200

    cambios

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
- hotfix/mejoras_ui
 
+# Título de la app
 st.title("Aplicación CSV Mejorada - Hotfix")
 
- 
-
-
-main
-
+# Subir archivo CSV
 archivo = st.file_uploader("Sube un CSV", type=["csv"])
 
+# Mostrar la tabla editable si se sube un CSV
 if archivo:
     df = pd.read_csv(archivo)
     st.experimental_data_editor(df, num_rows="dynamic")
